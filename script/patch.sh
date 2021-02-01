34ea8de9c426036fcf93683fe54bd12980e6a1ca
Peter Yoon <peyo.hd@gmail.com>
2021-02-01 09:11:13 +0900

    patch.sh : move most patches to local_manifests

--- script/patch.sh
+++ script/patch.sh
@@ -16,50 +16,11 @@ apply_zip(){
 	popd >> /dev/null
 }
 
-apply_patch art
-apply_patch bionic
-apply_patch build/make
-apply_patch build/soong
 apply_patch cts
-apply_patch dalvik
-apply_patch development
-apply_patch device/generic/goldfish
-apply_patch external/android-clat
-apply_patch external/boringssl
-apply_patch external/clang
-apply_patch external/compiler-rt
-apply_patch external/eigen
-apply_patch external/google-benchmark
-apply_patch external/libchrome
-apply_patch external/libjpeg-turbo
-apply_patch external/llvm
-apply_patch external/minijail
-apply_patch external/pdfium
-apply_patch external/perf_data_converter
-apply_patch external/protobuf
-apply_patch external/strace
-apply_patch external/tremolo
-apply_patch external/vixl
-apply_patch frameworks/av
-apply_patch frameworks/base
-apply_patch frameworks/native
-apply_patch hardware/interfaces
-apply_patch hardware/libhardware
-apply_patch libcore
-apply_patch libnativehelper
 apply_patch prebuilts/clang/host/linux-x86
 apply_patch prebuilts/ndk
 apply_patch prebuilts/vndk/v28
 apply_patch prebuilts/vndk/v27
-apply_patch system/apex
-apply_patch system/core
-apply_patch system/extras
-apply_patch system/libhidl
-apply_patch system/libhwbinder
-apply_patch system/vold
-apply_patch system/tools/hidl/
-apply_patch test/vts
-apply_patch test/vts-testcase/fuzz
 
 apply_zip prebuilts/gcc/linux-x86/riscv64/
 apply_zip prebuilts/misc
